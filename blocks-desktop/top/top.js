a4ab426cb9e50177f450199c3c0fd8f985ac8cd6
qerty <qerty@tadatuta.com>
2012-12-18 23:16:28 +0200

    scroll top

---
+++ blocks-desktop/top/top.js
@@ -0,0 +1,17 @@
+$(function() {
+
+function backTop(speed) {
+    $('body,html').animate({
+        scrollTop: 0
+    }, speed);
+    return false;
+}
+
+$(window).scroll(function () {
+        ($(this).scrollTop() > 350) ? $('.top').addClass('top_visiblity_visible') : $('.top').removeClass('top_visiblity_visible');
+    });
+
+    $('.top').mousedown(function(){
+        backTop(300);
+    }); // Хоть и в данном случае можно было функцию backTop выполнять прямо здесь.
+});
