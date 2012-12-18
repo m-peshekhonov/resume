c312de384ab7f5a47aab4fca5023f0cf6ce971f9
qerty <qerty@tadatuta.com>
2012-12-18 18:33:12 +0200

    first block

--- pages-desktop/index/index.bemjson.js
+++ pages-desktop/index/index.bemjson.js
@@ -1,11 +1,35 @@
 ({
     block: 'b-page',
-    title: 'clerBEM',
+    title: 'Резюме Михаила Пешехонова',
     head: [
         { elem: 'css', url: '_index.css', ie: false },
         { elem: 'css', url: '_index.ie.css', ie: 'lt IE 8' },
         { block: 'i-jquery', elem: 'core' },
         { elem: 'js', url: '_index.js' }
     ],
-    content: 'Tro-lo-lo'
+    content: [
+        {
+            elem: 'inner',
+            content: [
+                {
+                    block: 'content',
+                    content: [
+                        {
+                            elem: 'item',
+                            content: [
+                                {
+                                    elem: 'h4',
+                                    content: 'Фамилия, имя, отчество'
+                                },
+                                {
+                                    elem: 'text',
+                                    content: 'Пешехонов Михаил Александрович'
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
 })
