c9eb618de4bbe57c104b31dc54bc2ce9c0ffde75
m-peshekhonov <m-peshekhonov@tdtt-mac.local>
2012-12-18 15:06:39 +0200

    test page

---
+++ .bem/make.js
@@ -0,0 +1,16 @@
+//process.env.YENV = 'production';
+
+MAKE.decl('Arch', {
+
+    getLibraries: function() {
+
+        return {
+            'bem-bl': {
+                type: 'git',
+                url: 'git://github.com/bem/bem-bl.git',
+                treeish: '0.3'
+            }
+        };
+    }
+
+});
---
+++ pages-desktop/index/index.bemjson.js
@@ -0,0 +1,11 @@
+({
+    block: 'b-page',
+    title: 'clerBEM',
+    head: [
+        { elem: 'css', url: '_index.css', ie: false },
+        { elem: 'css', url: '_index.ie.css', ie: 'lt IE 8' },
+        { block: 'i-jquery', elem: 'core' },
+        { elem: 'js', url: '_index.js' }
+    ],
+    content: 'Tro-lo-lo'
+})
